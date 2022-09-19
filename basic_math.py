b195dad66aae065f02516e848fb014d55b5e7053
Joanne Santhosh <joanne.santhosh@mths.ca>
2022-09-19 15:25:33 +0000

    init

---
+++ basic_math.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Joanne Santhosh
+# Created on: Sept 2022
+# This is the Area and Perimeter program
+
+
+def main():
+    #This function calculates the area and perimeter
+
+    print("A rectangle has these dimensions: ")
+    print("5cm x 3cm")
+    print("")
+    print("The area is {}cm².".format(3 * 5))
+    print("The perimeter is {}cm.".format(2 * (3 + 5)))
+
+
+if __name__ == "__main__":
+    main()
